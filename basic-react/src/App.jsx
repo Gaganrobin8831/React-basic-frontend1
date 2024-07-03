877f606236be98a42fa9d737ebabb9c421fe10ae
Gagandeep singh <gaganrobin71@gmail.com>
2024-07-03 11:59:21 +0530

    do some changes

--- basic-react/src/App.jsx
+++ basic-react/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline shadow-md">
     Hello world!
   </h1>
   )
