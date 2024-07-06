a1db3c6e322ac6a5222b3c8399232de686f4377a
Gagandeep singh <gaganrobin71@gmail.com>
2024-07-06 12:49:09 +0530

    add some file

--- basic-react/src/component/HomePage.jsx
+++ basic-react/src/component/HomePage.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import Slider from './homeSlider'
+import React,{ useEffect, useRef } from 'react';
+import './HomePage.css';
+import Slider from './homeSlider';
+
 export default function Home() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 1; // Set the playback speed to 0.5x
+    }
+  }, []);
+
   return (
-    // <div className='w-full md:h-96 bg-blue-200 md:flex items-center 
-    //  sm:flex-wrap-reverse sm:h-screen '>
-    <video autoPlay loop muted>
-        <source src="/background-video.mp4" type="video/mp4" />
-    {/* <div className='h-96 md:w-6/12 sm:w-11/12'>
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi consequuntur quisquam quasi consequatur quod vel reprehenderit perferendis fugiat iste placeat?</div>
-    </div >
-
-
-    <div className='md:h-96 md:w-20 md:ml-40  md:flex md:items-center md:justify-evenly sm:flex sm:justify-center'>
-    <Slider/>
-    </div> */}
-    </video>
-    // </div>
+    <div className='backgroundimg'>
+      <video autoPlay loop muted className='video' ref={videoRef}>
+        <source src="/cycle.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className='content md:flex md:items-center md:justify-evenly sm:flex sm:items-center sm:justify-evenly'>
+        <div className='text'>
+          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi consequuntur quisquam quasi consequatur quod vel reprehenderit perferendis fugiat iste placeat?</div>
+        </div>
+        <div className='slider-container flex items-center justify-evenly'>
+          <Slider />  
+        </div>
+      </div>
+    </div>
   );
 }
