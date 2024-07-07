a720cb8149c4b495c8f4c0d41a6a4b7b64d35969
Gagandeep singh <gaganrobin71@gmail.com>
2024-07-07 12:33:05 +0530

    some changes

--- basic-react/src/component/About.jsx
+++ basic-react/src/component/About.jsx
@@ -6,7 +6,7 @@ export default function AboutPage() {
     <>
       <div className='aboutpage w-full h-full mt-16 mb-16 text-center'>
         <u className='text-blue-950'>
-          <div className='text-4xl mb-2 text-blue-950 font-extrabold'>
+          <div className='text-4xl mb-5 text-blue-950 font-extrabold'>
             About Us
           </div>
         </u>
@@ -14,13 +14,11 @@ export default function AboutPage() {
         <div className='image-container relative w-full h-80'>
           <img src="./3rd.jpg" className='aboutimg w-full h-full object-cover' />
           <div className='abouttext absolute inset-0 flex flex-col justify-center items-center text-white'>
-            <div className='text-start ml-12 mb-1 text-black font-bold'>
-              World Best Shop
+           
+            <div className='abouttext2 sm:text-lg md:text-4xl sm:font-bold  md:font-extrabold font-serif  text-blue-950  md:ml-10 mb-5'>
+              About Cycle Shop
             </div>
-            <div className='text-6xl font-bold font-serif text-blue-950 text-start ml-10 mb-5'>
-              Cycle Shop
-            </div>
-            <div className='text-wrap ml-10 w-3/4'>
+            <div className='text-wrap text-lg  ml-10 w-3/4'>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis beatae molestias ab voluptate fuga dolorem iure et quia ullam rem impedit magnam ipsam sint vel veniam omnis obcaecati unde animi, optio, odit repellat architecto. Pariatur numquam ea sed perferendis cumque?
             </div>
           </div>
--- basic-react/src/component/HomePage.jsx
+++ basic-react/src/component/HomePage.jsx
@@ -12,30 +12,30 @@ export default function Home() {
   }, []);
 
   return (
-    <div className='backgroundimg relative overflow-hidden'>
+    <div className='backgroundimg relative overflow-hidden sm:h-full'>
       <video autoPlay loop muted className='video' ref={videoRef}>
         <source src="/cycle.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
-      <div className='content md:flex md:items-center md:justify-evenly sm:flex sm:items-center sm:justify-evenly'>
+      <div className='content md:flex md:items-center md:justify-evenly sm:flex sm:items-center sm:justify-evenly sm:p-20'>
         
         
-        <div className='text mb-5 text-center border-solid ml-17 '>
-          <div className='text-start ml-12 mb-1 text-black font-bold'>World Best Shop</div>
-          <div className='text-6xl font-bold font-serif text-blue-950 text-start ml-10 mb-5'>
+        <div className='mb-5 text-center border-solid ml-17 sm:mt-4 flex-col items-center justify-evenly'>
+          <div className='md:text-start sm:text-center sm:mt-4 md:ml-12 md:mb-1  text-black font-bold'>World Best Shop</div>
+          <div className='text-6xl md:text-4xl sm:text-4xl font-bold font-serif text-blue-950  md:ml-10 mb-5 md:text-start sm:text-center '>
             Cycle Shop
         
           </div>
 
-          <div className='text-wrap ml-10 w-3/4'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis beatae molestias ab voluptate fuga dolorem iure et quia ullam rem impedit magnam ipsam sint vel veniam omnis obcaecati unde animi, optio, odit repellat architecto. Pariatur numquam ea sed perferendis cumque?
+          <div className='text-wrap ml-10 w-3/4 sm:text-xs md:text-start sm:text-center '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis beatae molestias ab voluptate fuga dolorem iure et quia ullam rem impedit magnam ipsam sint vel veniam omnis obcaecati unde animi, optio, odit repellat architecto. Pariatur numquam ea sed perferendis cumque?
 
           </div>
 
         </div>
 
 
-        <div className='slider-container flex items-center justify-evenly'>
+        <div className='slider-container flex items-center justify-evenly sm:p-20'>
           <Slider />  
         </div>
 
